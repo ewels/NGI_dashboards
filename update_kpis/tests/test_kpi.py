4bd21f14ae2055489a562be39bec26057372cbfc
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2016-02-09 10:55:44 +0100

    Added test cases for project KPIs

---
+++ update_kpis/tests/test_kpi.py
@@ -0,0 +1,71 @@
+from kpiupdater.kpi import *
+import yaml
+import unittest
+from datetime import datetime
+
+
+class TestProjectsKPI(unittest.TestCase):
+
+    def setUp(self):
+        with open("data/projects.yaml") as f:
+            projs = yaml.load(f)
+
+        self.p_iter = projs.itervalues()
+
+    def test_in_production(self):
+        p_inprod = ProjectsInProduction()
+        p_inprod.start_date = datetime(2016, 1, 1, 0, 0)
+        for doc in self.p_iter:
+            p_inprod(doc)
+        self.assertEqual(1, p_inprod.summary())
+
+    def test_in_application(self):
+        p_inapp = ProjectsInApplications()
+        p_inapp.start_date = datetime(2016, 1, 1, 0, 0)
+        for doc in self.p_iter:
+            p_inapp(doc)
+        self.assertEqual(2, p_inapp.summary())
+
+    def test_finished_lib(self):
+        p_finlib = ProjectsFinishedLib()
+        p_finlib.start_date = datetime(2016, 1, 1, 0, 0)
+        for doc in self.p_iter:
+            p_finlib(doc)
+        self.assertEqual(0, p_finlib.summary())
+
+    def test_libprep(self):
+        p_libprep = ProjectsLibraryPrep()
+        p_libprep.start_date = datetime(2016, 1, 1, 0, 0)
+        for doc in self.p_iter:
+            p_libprep(doc)
+        self.assertEqual(3, p_libprep.summary())
+
+    def test_closedseven(self):
+        p_cseven = ProjectsClosedLastSeven()
+        p_cseven.dnow = datetime(2016, 2, 8, 0, 0)
+        for doc in self.p_iter:
+            p_cseven(doc)
+        self.assertEqual(1, p_cseven.summary())
+
+
+    def test_openedseven(self):
+        p_oseven = ProjectsOpenedLastSeven()
+        p_oseven.dnow = datetime(2016, 2, 8, 0, 0)
+        for doc in self.p_iter:
+            p_oseven(doc)
+        self.assertEqual(1, p_oseven.summary())
+
+    def test_closedNweeks(self):
+        p_cnweeks = ProjectsClosedNWeeks()
+        p_cnweeks.dnow = datetime(2016, 2, 8, 0, 0)
+        for doc in self.p_iter:
+            p_cnweeks(doc)
+        self.assertEqual({0: 0, 1: 1, 2: 1, 3: 0}, p_cnweeks.summary())
+
+    def test_openedNweeks(self):
+        p_onweeks = ProjectsOpenedNWeeks()
+        p_onweeks.dnow = datetime(2016, 2, 8, 0, 0)
+        for doc in self.p_iter:
+            p_onweeks(doc)
+        self.assertEqual({0: 0, 1: 1, 2: 0, 3: 1}, p_onweeks.summary())
+
