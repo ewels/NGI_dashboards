38e8440cdac1538e06f43e565a6a880d5685cb60
Phil Ewels <phil.ewels@scilifelab.se>
2016-09-02 14:34:30 +0200

    Got Num Projects plot working with multiple years.

--- make_dashboards/templates/external/js/dashboard.js
+++ make_dashboards/templates/external/js/dashboard.js
@@ -3,138 +3,121 @@
 
 $(function () {
     
-    Highcharts.setOptions({
-        chart: {
-            style: {
-                fontFamily:'"Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif'
+    try {
+    
+        Highcharts.setOptions({
+            chart: {
+                style: {
+                    fontFamily:'"Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif'
+                }
+            }
+        });
+        
+        // Header clock
+        updateClock();
+        
+        // Format KPI update number
+        $('#time_created').text(moment($('#time_created').text()).format("YYYY-MM-DD, HH:mm"));
+        
+        // Cron job runs on the hour, every hour. Get the web page to reload at 5 past the next hour
+        var reloadDelay = moment().add(1, 'hours').startOf('hour').add(5, 'minutes').diff();
+        setTimeout(function(){ location.reload(); }, reloadDelay );
+        console.log("Reloading page in "+Math.floor(reloadDelay/(1000*60))+" minutes");
+        
+        // Projects plot
+        var num_years = 2;
+        var p_cutoff = 5;
+        var years = Object.keys(data['num_projects']).sort().reverse();
+        num_projects_cats = Array();
+        num_projects_data = Array();
+        // Find the total counts for all years
+        var dtotal = Array();
+        for(var i=0; i<num_years; i++){
+            for(k in data['num_projects'][years[i]]){
+                if(dtotal[k] == undefined){
+                    dtotal[k] = data['num_projects'][years[i]][k];
+                } else {
+                    dtotal[k] += data['num_projects'][years[i]][k];
+                }
             }
         }
-    });
-    
-    // Header clock
-    updateClock();
-    
-    // Top Row
-    make_bar_plot('#lp_queue', [], [], 'green');
-    make_bar_plot('#seq_queue', [], [], 'blue');
-    make_bar_plot('#analysis_queue', [], [], 'blue');
-    
-    // Bottom row
-    make_bar_plot('#user_affiliations', [], [], 'green');
-    make_bar_plot('#throughput_plot', [], [], 'green');
-    delivery_times_plot('#delivery_times', 80);
-    seq_yield_plot('#seq_yield');
+        var num_projects_cats = Object.keys(dtotal).sort(function(a,b){return dtotal[a]-dtotal[b]}).reverse();
+        // Get the data
+        for(var i=(num_years-1); i>=0; i--){
+            var year = years[i];
+            var yeardata = Array();
+            for(j=0; j<num_projects_cats.length; j++){
+                if(dtotal[num_projects_cats[j]] >= p_cutoff){
+                    if(data['num_projects'][year][num_projects_cats[j]] == undefined){
+                        yeardata.push(0);
+                    } else {
+                        yeardata.push(data['num_projects'][year][num_projects_cats[j]]);
+                    }
+                }
+            }
+            num_projects_data.push({
+                'name': year,
+                'data': yeardata,
+                'dataLabels': {
+                    'enabled': i == 0
+                }
+            });
+        }
+        make_bar_plot('#num_projects_plot', num_projects_cats, num_projects_data);
+        
+    } catch(err){
+        $('.main-page').html('<div class="alert alert-danger text-center" style="margin: 100px 50px;"><p><strong>Error loading dashboard data</strong></p></div><pre style="margin: 100px 50px;"><code>'+err+'</code></pre>');
+        console.log(err);
+        // Try reloading in a few minutes
+        setTimeout(function(){ location.reload(); }, 300000); // 5 minutes
+    }
     
 });
 
 
-function make_bar_plot(target, data, categories, colour){
-    $(target).highcharts({
-        chart: {
-            type: 'column',
-            backgroundColor:'rgba(255, 255, 255, 0.1)',
-            height: 200,
-            spacingBottom: 0,
-        },
-        title: { text: null },
-        credits: { enabled: false },
-        xAxis: [{
-            categories: categories,
-            reversed: false,
-            labels: {
-                step: 1
-            }
-        }],
-        yAxis: {
-            title: {
-                text: '#Projects'
-            }
-        },
-        plotOptions: {
-            series: {
-                stacking: 'normal'
-            }
-        },
-        legend: {
-            reversed: true,
-            floating: true,
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            labelFormat: '<span style="font-weight: 300;">{name}</span>',
-            useHTML: true
-        },
-        series: [{
-            name: 'Closed',
-            data: data
-        }, {
-            name: 'Opened',
-            data: data
-        }]
-    });
-}
-
-// Make an arc plot to show percentage success
-function delivery_times_plot(target, now){
-    $(target).highcharts({
-        chart: {
-            type: 'solidgauge',
-            backgroundColor:'rgba(255, 255, 255, 0.1)',
-            height: 180
-        },
-        title: null,
-        pane: {
-            startAngle: -90,
-            endAngle: 90,
-            background: {
-                backgroundColor: '#EEE',
-                innerRadius: '60%',
-                outerRadius: '100%',
-                shape: 'arc'
+// Make a bar plot
+function make_bar_plot(target, categories, data, title){
+    try {
+        if(target === undefined){ throw 'Target missing'; }
+        if(categories === undefined){ throw 'Categories missing'; }
+        $(target).highcharts({
+            chart: {
+                type: 'bar',
+                height: 500,
+                backgroundColor:'rgba(255, 255, 255, 0.1)'
             },
-            center: ['50%', '70%'],
-            size: 200
-        },
-        tooltip: { enabled: false },
-        credits: { enabled: false },
-
-        // the value axis
-        yAxis: {
-            stops: [
-                [0.1, '#DF5353'], // green
-                [0.5, '#DDDF0D'], // yellow
-                [0.9, '#55BF3B'] // red
-            ],
-            lineWidth: 0,
-            minorTickInterval: null,
-            tickPixelInterval: 400,
-            tickWidth: 0,
-            labels: {
-                enabled: false
+            tooltip: { enabled: false },
+            credits: { enabled: false },
+            xAxis: {
+                categories: categories
             },
-            min: 0,
-            max: 100
-        },
-        plotOptions: {
-            solidgauge: {
-                dataLabels: {
-                    y: -40,
-                    borderWidth: 0,
-                    useHTML: true
+            yAxis: {
+                min: 0,
+            },
+            legend: {
+                reversed: true,
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'bottom',
+                x: -20,
+                y: -60,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            plotOptions: {
+                bar: {
+                    borderWidth: 0
                 }
-            }
-        },
-        series: [{
-            name: 'Success Rate',
-            data: [now],
-            dataLabels: { format: '<div style="text-align:center; font-size:25px; color:black; font-weight: normal;">{y}%</div>' },
-        }]
-    });
+            },
+            series: data
+        });
+    } catch(err) {
+        $(target).addClass('coming_soon').text('coming soon');
+    }
 }
 
-function seq_yield_plot(){
-    
-}
 
 function updateClock(){
     var now = moment(),
@@ -147,5 +130,9 @@ function updateClock(){
     $('#second').css("transform", "rotate(" + second + "deg)");
     $('#clock_time').text( moment().format('HH:mm') );
     $('#clock_date').text( moment().format('dddd Do MMMM') );
+    
+    var updated = moment($('#time_created').data('original'));
+    $('#report_age').text( moment().from(updated, true) );
     setTimeout(updateClock, 1000);
 }
+
