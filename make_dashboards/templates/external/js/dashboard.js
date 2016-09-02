8a873accf4f9ac3cee90f63304bef364126aebe9
Phil Ewels <phil.ewels@scilifelab.se>
2016-09-02 15:56:45 +0200

    Got delivery times plot working.

--- make_dashboards/templates/external/js/dashboard.js
+++ make_dashboards/templates/external/js/dashboard.js
@@ -27,40 +27,19 @@ $(function () {
         // # Projects plot
         var years = Object.keys(data['num_projects']).sort().reverse();
         var ydata = data['num_projects'][years[0]];
-        var cats = Object.keys(ydata).sort(function(a,b){return ydata[a]-ydata[b]}).reverse();
-        var sorted_ydata = Array();
-        var num_projects_cats = Array();
-        for(j=0; j<cats.length; j++){
-            if(data['key_names'][cats[j]] == undefined){
-                num_projects_cats.push(cats[j]);
-            } else {
-                num_projects_cats.push(data['key_names'][cats[j]]);
-            }
-            sorted_ydata.push(ydata[cats[j]]);
-        }
-        var num_projects_data = [{ name: years[0], data: sorted_ydata }];
-        $('#num_projects_heading').text('# Projects in '+years[0]);
-        make_bar_plot('#num_projects_plot', num_projects_cats, num_projects_data);
+        make_bar_plot('#num_projects_plot', ydata, '# Projects in '+years[0]);
         
         // # Samples plot
         var years = Object.keys(data['num_samples']).sort().reverse();
         var ydata = data['num_samples'][years[0]];
-        var cats = Object.keys(ydata).sort(function(a,b){return ydata[a]-ydata[b]}).reverse();
-        var sorted_ydata = Array();
-        var num_samples_cats = Array();
-        for(j=0; j<cats.length; j++){
-            if(data['key_names'][cats[j]] == undefined){
-                num_samples_cats.push(cats[j]);
-            } else {
-                num_samples_cats.push(data['key_names'][cats[j]]);
-            }
-            sorted_ydata.push(ydata[cats[j]]);
-        }
-        var num_samples_data = [{ name: years[0], data: sorted_ydata }];
-        console.log(num_samples_cats);
-        console.log(num_samples_data);
-        $('#num_samples_heading').text('# Samples in '+years[0]);
-        make_bar_plot('#num_samples_plot', num_samples_cats, num_samples_data);
+        make_bar_plot('#num_samples_plot', ydata, '# Samples in '+years[0]);
+        
+        // Open Projects plot
+        var ydata = data['open_projects'];
+        make_bar_plot('#open_projects_plot', ydata, '# Open Projects ');
+        
+        // Delivery times plot
+        make_delivery_times_plot();
         
     } catch(err){
         $('.main-page').html('<div class="alert alert-danger text-center" style="margin: 100px 50px;"><p><strong>Error loading dashboard data</strong></p></div><pre style="margin: 100px 50px;"><code>'+err+'</code></pre>');
@@ -73,23 +52,43 @@ $(function () {
 
 
 // Make a bar plot
-function make_bar_plot(target, categories, data, title){
+function make_bar_plot(target, ydata, title){
     try {
         if(target === undefined){ throw 'Target missing'; }
-        if(categories === undefined){ throw 'Categories missing'; }
+        if(ydata === undefined){ throw 'Data missing'; }
+        if(title === undefined){ title = null; }
+        
+        var cats = Object.keys(ydata).sort(function(a,b){return ydata[a]-ydata[b]}).reverse();
+        var sorted_ydata = Array();
+        var nice_cats = Array();
+        var total_count = 0;
+        for(j=0; j<cats.length; j++){
+            if(data['key_names'][cats[j]] == undefined){
+                nice_cats.push(cats[j]);
+            } else {
+                nice_cats.push(data['key_names'][cats[j]]);
+            }
+            sorted_ydata.push(ydata[cats[j]]);
+            total_count += ydata[cats[j]];
+        }
+        
         $(target).highcharts({
             chart: {
                 type: 'bar',
                 height: 300,
-                backgroundColor:'rgba(255, 255, 255, 0.1)'
+                plotBackgroundColor: null,
             },
             title: {
-                text: null
+                text: title,
+                style: { 'font-size': '24px' }
+            },
+            subtitle: {
+                text: 'Total: '+total_count
             },
             tooltip: { enabled: false },
             credits: { enabled: false },
             xAxis: {
-                categories: categories
+                categories: nice_cats
             },
             yAxis: {
                 min: 0,
@@ -104,11 +103,69 @@ function make_bar_plot(target, categories, data, title){
                     dataLabels: { enabled: true }
                 },
             },
-            series: data
+            series: [{ data: sorted_ydata }]
         });
     } catch(err) {
         $(target).addClass('coming_soon').text('coming soon');
+        console.log(err);
     }
+}
+
+
+function make_delivery_times_plot(){
+    var years = Object.keys(data['delivery_times']).sort().reverse();
+    var ydata = data['delivery_times'][years[0]];
+    var ykeys = Object.keys(ydata).sort(function(a,b){ return a.match(/\d+/)-b.match(/\d+/) });
+    var pdata = Array();
+    for(i=0; i<ykeys.length; i++){ pdata.push([ykeys[i], ydata[ykeys[i]]]); }
+    
+    $('#delivery_times_plot').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false,
+            height: 300,
+        },
+        title: {
+            text: 'Delivery Times in '+years[0],
+            style: { 'font-size': '24px' }
+        },
+        tooltip: { enabled: false },
+        credits: { enabled: false },
+        plotOptions: {
+            pie: {
+                // dataLabels: { enabled: false },
+                dataLabels: {
+                    enabled: true,
+                    formatter: function() {
+                        return Math.round(this.percentage*100)/100 + ' %';
+                    },
+                    distance: -40,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black',
+                        'font-size': '18px'
+                    }
+                },
+                showInLegend: true,
+                startAngle: -90,
+                endAngle: 90,
+                size: '150%',
+                center: ['50%', '75%']
+            }
+        },
+        legend: {
+            enabled: true,
+            floating: true
+        },
+        series: [{
+            type: 'pie',
+            name: 'Delivery Times',
+            innerSize: '50%',
+            data: pdata
+        }]
+    });
 }
 
 
